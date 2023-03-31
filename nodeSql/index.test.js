c0a7a23f9355fc3b920dc2b6b61881dcece64b7d
krisztiann3 <97217377+krisztiann3@users.noreply.github.com>
2023-03-31 11:47:06 +0200

    Unit test próbák
    sikertelen

--- nodeSql/index.test.js
+++ nodeSql/index.test.js
@@ -1,6 +1,40 @@
-const login = require('/login')
+const assert = require('chai').assert;
+const request = require('supertest');
+const {app} = require('./index');
 
-test('bejelentkezés sikeres-e', () => {
-    expect(login.add('nev')).toBe(nev);
-    expect(login.add('jelszo')).toBe(jelszo);
-})
+
+
+describe('login', function() {
+    it('Megfelelő adatok estén kiosztja a tokent és a bejelentkezés sikeres', function(done) {
+      request(app)
+        post('/login')
+        .send({ nev: 'test', jelszo: 'password' })
+        .end(function(err, res) {
+          assert.equal(res.status, 200);
+          assert.isString(res.body.token);
+          done();
+        });
+    });
+  
+    it('Error-t dob vissza ha a bejelentkezés sikertelen', function(done) {
+      request(app)
+        .post('localhost:8080/login/')
+        .send({ nev: 'test', jelszo: 'wrongpassword' })
+        .end(function(err, res) {
+          assert.equal(res.status, 401);
+          assert.equal(res.body.message, 'Sikertelen bejelentkezés');
+          done();
+        });
+    });
+  
+    it('Error-t dob vissza ha hiányoznak adatok', function(done) {
+      request(app)
+        .post('/login')
+        .end(function(err, res) {
+          assert.equal(res.status, 400);
+          assert.equal(res.body.message, 'hibás adatok');
+          done();
+        });
+    });
+  });
+  
